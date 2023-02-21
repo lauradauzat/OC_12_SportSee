343e8b17562a017708d722889755c56e6e9b3b05
Laura Dauzat <laura.rzp@gmail.com>
2023-02-21 22:57:20 +0100

    api connexion

--- Frontend/sportsee/src/App.js
+++ Frontend/sportsee/src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 //import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
 
@@ -10,10 +10,24 @@ import './style.css';
 
 
 function App() {
+
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      async function fetchData() {
+        const response = await fetch('http://localhost:3001/user/18');
+        const json = await response.json();
+        setData(json);
+      }
+      fetchData();
+    }, []);
+
+
   return (
     <>
       <Navbar></Navbar>
-      <Main></Main>
+      <Main data={data}></Main>
     </>
     
   );
--- Frontend/sportsee/src/components/Main.jsx
+++ Frontend/sportsee/src/components/Main.jsx
@@ -1,11 +1,16 @@
-function Main() {
+function Main({...data}) {
+
+    console.log(data.data.data);
+
+
     return (
         <>
         <main>
         <div className="intro">
           <h1>
-            Bonjour
-            <span className="red">Name</span>
+            Bonjour 
+         
+          <span className="red">  {data.data.data.userInfos.firstName}</span> 
           </h1>
           <p>
             Félicitation ! Vous avez explosé vos objectifs hier 👏
@@ -17,14 +22,14 @@ function Main() {
           <div className="cal white-bg">
             <img src="/img/calories-icon.png" alt="calories" />
             <div className="right-text">
-              <span id="cal-data">...g</span>
+            <span id="cal-data">{data.data.data.keyData.calorieCount} g</span>
               <p>Calories</p>
             </div>
           </div>
           <div className="prot white-bg">
             <img src="/img/protein-icon.png" alt="protein" />
             <div className="right-text">
-              <span id="cal-prot">...g</span>
+            <span id="cal-prot">{data.data.data.keyData.proteinCount} g</span> 
               <p>Protéines</p>
             </div>
           </div>
@@ -34,14 +39,14 @@ function Main() {
           <div className="gluc white-bg">
             <img src="/img/carbs-icon.png" alt="carbs" />
             <div className="right-text">
-              <span id="gluc-data">...g</span>
+              <span id="gluc-data">{data.data.data.keyData.carbohydrateCount} g</span>
               <p>Glucides</p>
             </div>
           </div>
           <div className="lip white-bg">
             <img src="/img/fat-icon.png" alt="fat" />
             <div className="right-text">
-              <span id="lip-data">...g</span>
+              <span id="lip-data">{data.data.data.keyData.lipidCount} g</span>
               <p>Lipides</p>
             </div>
           </div>
