fd571e7b44ce6fa2336f35a0496f5b4cd879b96a
Laura Dauzat <laura.rzp@gmail.com>
2023-02-22 16:56:04 +0100

    install recharts

--- Frontend/sportsee/src/components/Main.jsx
+++ Frontend/sportsee/src/components/Main.jsx
@@ -1,3 +1,5 @@
+import ActiviteQuotidienne from "./ActiviteQuotidienne";
+
 function Main({...data}) {
 
     console.log(data.data.data);
@@ -10,7 +12,7 @@ function Main({...data}) {
           <h1>
             Bonjour 
          
-          <span className="red">  {data.data.data.userInfos.firstName}</span> 
+        {/*   <span className="red">  {data.data.data.userInfos.firstName}</span>  */}
           </h1>
           <p>
             Félicitation ! Vous avez explosé vos objectifs hier 👏
@@ -18,18 +20,18 @@ function Main({...data}) {
         </div>
 
         <div className="charts-container">
-          <div className="actquot white-bg"></div>
+          <ActiviteQuotidienne></ActiviteQuotidienne>
           <div className="cal white-bg">
             <img src="/img/calories-icon.png" alt="calories" />
             <div className="right-text">
-            <span id="cal-data">{data.data.data.keyData.calorieCount} g</span>
+         {/*    <span id="cal-data">{data.data.data.keyData.calorieCount} g</span> */}
               <p>Calories</p>
             </div>
           </div>
           <div className="prot white-bg">
             <img src="/img/protein-icon.png" alt="protein" />
             <div className="right-text">
-            <span id="cal-prot">{data.data.data.keyData.proteinCount} g</span> 
+        {/*     <span id="cal-prot">{data.data.data.keyData.proteinCount} g</span>  */}
               <p>Protéines</p>
             </div>
           </div>
@@ -39,14 +41,14 @@ function Main({...data}) {
           <div className="gluc white-bg">
             <img src="/img/carbs-icon.png" alt="carbs" />
             <div className="right-text">
-              <span id="gluc-data">{data.data.data.keyData.carbohydrateCount} g</span>
+          {/*     <span id="gluc-data">{data.data.data.keyData.carbohydrateCount} g</span> */}
               <p>Glucides</p>
             </div>
           </div>
           <div className="lip white-bg">
             <img src="/img/fat-icon.png" alt="fat" />
             <div className="right-text">
-              <span id="lip-data">{data.data.data.keyData.lipidCount} g</span>
+        {/*       <span id="lip-data">{data.data.data.keyData.lipidCount} g</span> */}
               <p>Lipides</p>
             </div>
           </div>
